3b7c1fc39f454a2bd66c87e767695a33dd4ab658
NoffarLevi <noffarlevi@gmail.com>
2020-12-28 14:08:51 +0200

    added appointment details page for BO added profile picture from camera

--- app/src/main/java/com/example/nailscheduler/BoAppointmentDetails.java
+++ app/src/main/java/com/example/nailscheduler/BoAppointmentDetails.java
@@ -1,21 +1,16 @@
 package com.example.nailscheduler;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.nailscheduler.models.CityJSON;
-import com.example.nailscheduler.services.CitiesAdapter;
 import com.example.nailscheduler.services.FirebaseStorageManage;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,48 +19,64 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+public class BoAppointmentDetails extends AppCompatActivity {
 
-public class ClientAppointmentDetails extends AppCompatActivity {
-
-    String currentBO;
+    String currentCL;
     String aptKey;
     public DatabaseReference mRefApt, mRefBo ;
-    String aptDate, aptStart, aptEnd, boName, boNumber, boCity, boStreet, boNumAd, nailImage;
-    private TextView dateTxtView, timeTxtView, boNameTxtView, boPhoneTxtView, boAddressTxtView;
+    String aptDate, aptStart, aptEnd, clName, clNumber, nailImage;
+    private TextView dateTxtView, timeTxtView, clNameTxtView, clPhoneTxtView;
     private ImageView nailExImgView;
     private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.client_appointment_details);
+        setContentView(R.layout.activity_bo_appointment_details);
         dateTxtView = findViewById(R.id.date);
         timeTxtView = findViewById(R.id.time);
-        boNameTxtView = findViewById(R.id.businessName);
-        boPhoneTxtView = findViewById(R.id.businessPhoneNumber);
-        boAddressTxtView = findViewById(R.id.businessAddress);
-        nailExImgView = findViewById(R.id.nailExample);
+        clNameTxtView = findViewById(R.id.clientName);
+        clPhoneTxtView = findViewById(R.id.clientPhoneNumber);
+        nailExImgView = findViewById(R.id.nailExmpl);
 
         Intent intent = getIntent();
-        currentBO = intent.getExtras().getString("businessOwner");
         aptKey = intent.getExtras().getString("appointmentKey");
 
         mRefApt = FirebaseDatabase.getInstance().getReference().getRoot().child("Appointments").child(aptKey);
         mRefApt.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
+                currentCL = dataSnapshot.child("clientID").getValue().toString();
                 aptDate = dataSnapshot.child("date").getValue().toString();
                 aptStart = dataSnapshot.child("startTime").getValue().toString();
                 aptEnd = dataSnapshot.child("endTime").getValue().toString();
                 Task<Uri> downloadUri = FirebaseStorageManage.getAppointmentImage(aptKey, FirebaseStorageManage.APT_IMAGE_EXAMPLE);
-                if(downloadUri.isComplete()){
+                if (downloadUri.isComplete()) {
                     nailImage = downloadUri.getResult().toString();
                     Picasso.get().load(nailImage).into(nailExImgView);
                 }
                 dateTxtView.setText(aptDate);
                 timeTxtView.setText(aptStart + ":00" + " - " + aptEnd + ":00");
+                second();
+            }
+
+            private void second() {
+                mRefBo = FirebaseDatabase.getInstance().getReference().getRoot().child("Clients").child(currentCL);
+                mRefBo.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        clName = snapshot.child("fullName").getValue().toString();
+                        clNumber = snapshot.child("phoneNumber").getValue().toString();
+
+                        clNameTxtView.setText(clName);
+                        clPhoneTxtView.setText(clNumber);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
 
             @Override
@@ -84,27 +95,6 @@ public class ClientAppointmentDetails extends AppCompatActivity {
                         }
                     }
                 });
-
-
-            mRefBo = FirebaseDatabase.getInstance().getReference().getRoot().child("BusinessOwners").child(currentBO);
-            mRefBo.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    boName = snapshot.child("businessName").getValue().toString();
-                    boNumber = snapshot.child("phoneNumber").getValue().toString();
-                    boCity = snapshot.child("boAddress").child("cityName").getValue().toString();
-                    boStreet = snapshot.child("boAddress").child("street").getValue().toString();
-                    boNumAd = snapshot.child("boAddress").child("number").getValue().toString();
-
-                    boNameTxtView.setText(boName);
-                    boPhoneTxtView.setText(boNumber);
-                    boAddressTxtView.setText(boStreet+ " " + boNumAd + ", " +boCity);
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
     }
+
 }
